4743ccbacdc8d5fc119471047b5d6df119f7863d
sridharpatnayak <sridharpatnayak@gmail.com>
2017-07-03 07:18:39 +0530

    Initial-1

--- src/SampleGitHubDeployment/Startup.cs
+++ src/SampleGitHubDeployment/Startup.cs
@@ -30,7 +30,7 @@ namespace SampleGitHubDeployment
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!Deployment");
             });
         }
     }
